77fa61ce86e98c1b793bf3327eec243f47d235f4
Satendra gangwar <gsatendra488@gmail.com>
2022-07-09 01:49:02 +0530

    routes create

--- server.js
+++ server.js
@@ -13,19 +13,9 @@ app.use(expressLayout)
 app.set('views' , path.join(__dirname,'/resources/views'))
 app.set('view engine' ,'ejs')
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+require('./routes/web')(app)
 
-app.get('/cart' ,(req,res)=>{
-    res.render('./customer/cart')
-})
-app.get('/login' ,(req,res)=>{
-    res.render('./auth/login')
-})
-app.get('/register' ,(req,res)=>{
-    res.render('./auth/register')
-})
+
 
 app.listen(PORT,(req,res)=>{
      console.log("connected")
